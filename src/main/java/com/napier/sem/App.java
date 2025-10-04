59229ef07068f189e140928f7331b4a73e83041e
RutaMNapier <40783212@live.napier.ac.uk>
2025-10-04 16:26:39 +0100

    Database connection is working
    Signed-off-by: Ruta Minkstimaite <30101934@nescol.ac.uk>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,111 +2,61 @@ package com.napier.sem;
 
 import java.sql.*;
 
-/**
- * Main application to connect to MySQL and query employee data
- */
-public class App {
-    /**
-     * Connection to MySQL database.
-     */
-    private Connection con = null;
-
-    /**
-     * Connect to the MySQL database.
-     */
-    public void connect() {
-        try {
+public class App
+{
+    public static void main(String[] args)
+    {
+        try
+        {
             // Load Database driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
-        int retries = 10;
-        for (int i = 0; i < retries; ++i) {
+        // Connection to the database
+        Connection con = null;
+        int retries = 100;
+        for (int i = 0; i < retries; ++i)
+        {
             System.out.println("Connecting to database...");
-            try {
+            try
+            {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection(
-                        "jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false",
-                        "root",
-                        "example"
-                );
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
+                // Wait a bit
+                Thread.sleep(10000);
+                // Exit for loop
                 break;
-            } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + i);
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
+            }
+            catch (InterruptedException ie)
+            {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
-    }
 
-    /**
-     * Disconnect from the MySQL database.
-     */
-    public void disconnect() {
-        if (con != null) {
-            try {
+        if (con != null)
+        {
+            try
+            {
                 // Close connection
                 con.close();
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 System.out.println("Error closing connection to database");
             }
         }
-    }
-
-    /**
-     * Get an employee record by ID
-     */
-    public Employee getEmployee(int ID) {
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // SQL query string
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            if (rset.next()) {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
-                emp.first_name = rset.getString("first_name");
-                emp.last_name = rset.getString("last_name");
-                return emp;
-            } else {
-                return null;
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-            return null;
-        }
-    }
-
-    public static void main(String[] args) {
-        // Create new Application
-        App a = new App();
-
-        // Connect to database
-        a.connect();
-
-        // Example: fetch employee 10001
-        Employee emp = a.getEmployee(10001);
-        if (emp != null) {
-            System.out.println(emp.emp_no + " " + emp.first_name + " " + emp.last_name);
-        } else {
-            System.out.println("Employee not found");
-        }
-
-        // Disconnect from database
-        a.disconnect();
     }
 }
