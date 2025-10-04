522d3cbc35be0b7acbf2e642874ef74326fee044
RutaMNapier <40783212@live.napier.ac.uk>
2025-10-04 17:59:58 +0100

    Getting an employee
    Signed-off-by: Ruta Minkstimaite <30101934@nescol.ac.uk>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -85,7 +85,59 @@ public class App
         // Connect to database
         a.connect();
 
+        // Retrieve one employee
+        Employee emp = a.getEmployee(10001);
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name);
+        }
+        else
+        {
+            System.out.println("Employee not found");
+        }
+
         // Disconnect from database
         a.disconnect();
     }
+
+
+    /**
+     * Gets an employee’s basic details by ID.
+     */
+    public Employee getEmployee(int ID)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT emp_no, first_name, last_name "
+                            + "FROM employees "
+                            + "WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid
+            if (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            }
+            else
+                return null;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
+    }
+
 }
